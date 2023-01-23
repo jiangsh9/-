b82d3fed609c30013c28a3943c7857e3374ee4c7
Shaohua Jiang <jiangshaohua.j@gmail.com>
2023-01-23 15:33:35 -0800

    Afternoon push

--- 项目2(ing)-客户申请保单/code/所有html合并尝试/my code/js/multi_step_form.js
+++ 项目2(ing)-客户申请保单/code/所有html合并尝试/my code/js/multi_step_form.js
@@ -1,3 +1,5 @@
+// 以下是 jQuery 相关的 一些 functions
+
 $(document).ready(function() {
 
     /*---------------------------------------------------------*/
@@ -17,12 +19,19 @@ $(document).ready(function() {
 
 });
 
+
+
+
+
+
+// 以下是 常规javascript 的一些 functions
+
+// 在 "受保人是否超过60岁" 时 选 "是" 以后，显示新信息 的代码
 function unhide_60(id){
-//   var insured_elder_id=document.getElementById(id);
-//   insured_elder_id.style.display="block";
     document.getElementById(id).style.display="block";
 }
 
+// 在 "受保人是否超过60岁" 时 选 "否" 以后，隐藏新信息 的代码
 function rehide_60(id){
     document.getElementById(id).style.display="none";
 }
